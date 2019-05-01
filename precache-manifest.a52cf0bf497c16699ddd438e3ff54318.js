8f59dc421d62bc853c6b02caffd909e0e645dbbd
mpabarca <mpabarca@uc.cl>
2019-05-01 16:34:52 -0400

    Updates

---
+++ precache-manifest.a52cf0bf497c16699ddd438e3ff54318.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9263c872eb1965a2b1e5c945e3875e48",
+    "url": "/BurgerQueen/index.html"
+  },
+  {
+    "revision": "720ea04bcb75886f709d",
+    "url": "/BurgerQueen/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "60e09b0c6f1f18c08c57",
+    "url": "/BurgerQueen/static/css/main.c18e4ed3.chunk.css"
+  },
+  {
+    "revision": "720ea04bcb75886f709d",
+    "url": "/BurgerQueen/static/js/2.359f7023.chunk.js"
+  },
+  {
+    "revision": "60e09b0c6f1f18c08c57",
+    "url": "/BurgerQueen/static/js/main.0bab8fef.chunk.js"
+  },
+  {
+    "revision": "d572bac6b25c7307f146",
+    "url": "/BurgerQueen/static/js/runtime~main.a15aad73.js"
+  }
+]);
